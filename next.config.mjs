86ab07a9d3b8e284e5e0a9ec897f1dcc3607c310
Sithu <sithuaungkhant@codigo.co>
2025-06-15 10:32:01 +0630

    feat: update product images and details, enhance image domain support
    - Added new image domains to the Next.js configuration for improved image loading.
    - Updated product details including names, images, descriptions, categories, materials, and tags for better accuracy and relevance.
    - Removed obsolete product entries and ensured consistency in data formatting.

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['randomuser.me'],
+    domains: [
+      'randomuser.me',
+      'salve.co.in',
+      'www.bsa-images.com',
+      'www.my-jewellery.com',
+      'i00.eu',
+      'images.laceandfavour.com',
+      'www.tanishq.co.in',
+    ],
   },
 };
 
