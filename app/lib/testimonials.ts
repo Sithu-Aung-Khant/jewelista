93624dd408f346a008a6fdd47f5418389ae33e3a
sithu <sithu.sithuthedev@gmail.com>
2025-08-22 20:44:41 +0630

    refactor(testimonials): update customer images and names for consistency
    - Changed image types from PNG to JPG and updated image paths for all testimonials.
    - Revised customer names to reflect accurate representation.
    - Removed outdated testimonials to streamline the content.

--- app/lib/testimonials.ts
+++ app/lib/testimonials.ts
@@ -1,28 +1,25 @@
 export const testimonials = [
   {
     id: 1,
-    imageType: 'png',
-    image: '/images/customers/1.png',
-    name: 'Amira K',
-    position: 'Product Quality Engineer',
+    imageType: 'jpg',
+    image: '/images/customers/1.jpg',
+    name: 'Swan Htet Naing',
     description:
       'Absolutely breathtaking! The craftsmanship of my ring from Jewelista exceeded all my expectations. The attention to detail is simply remarkable.',
   },
   {
     id: 2,
-    imageType: 'png',
-    image: '/images/customers/2.png',
-    name: 'Sophia L',
-    position: 'Product Quality Engineer',
+    imageType: 'jpg',
+    image: '/images/customers/2.jpg',
+    name: 'Hsu Lae May',
     description:
       'From the moment I stepped into Jewelista, I felt like royalty. Their personalized service and exquisite collection made choosing my jewelry a magical experience.',
   },
   {
     id: 3,
-    imageType: 'png',
-    image: '/images/customers/3.png',
-    name: 'Rania M',
-    position: 'Product Quality Engineer',
+    imageType: 'jpg',
+    image: '/images/customers/3.jpg',
+    name: 'Shwecin Winn Nu',
     description:
       'Every detail at Jewelista, from the sparkle of the diamonds to the elegant packaging, speaks of luxury and sophistication. A truly premium jewelry experience.',
   },
@@ -30,27 +27,8 @@ export const testimonials = [
     id: 4,
     imageType: 'jpg',
     image: '/images/customers/4.jpg',
-    name: 'Daniel R Langosh',
-    position: 'Product Quality Engineer',
+    name: 'Su Su Yee',
     description:
       "Jewelista doesn't just sell jewelry - they create lasting memories. Their exceptional quality and outstanding service made choosing an engagement ring a wonderful experience.",
-  },
-  {
-    id: 5,
-    imageType: 'png',
-    image: '/images/customers/1.png',
-    name: 'Amira K',
-    position: 'Product Quality Engineer',
-    description:
-      "The expertise and guidance from Jewelista's team helped me find the perfect piece. Their collection is stunning and the quality is unmatched.",
-  },
-  {
-    id: 6,
-    imageType: 'png',
-    image: '/images/customers/2.png',
-    name: 'Sophia L',
-    position: 'Product Quality Engineer',
-    description:
-      "Jewelista's attention to detail and commitment to excellence is evident in every piece. I couldn't be happier with my purchase and the entire shopping experience.",
   },
 ];
